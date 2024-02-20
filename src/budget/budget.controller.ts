9ba725026f66d64e86da68520db747ee634d9a46
Teerut Srithongdee <teerutsr@gmail.com>
2024-02-20 18:22:53 +0700

    Add @nestjs/swagger package

--- src/budget/budget.controller.ts
+++ src/budget/budget.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { BudgetService } from './budget.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Budget API")
 @Controller('budget')
+
 export class BudgetController {
   constructor(private readonly budgetService: BudgetService) {}
   @Get()
